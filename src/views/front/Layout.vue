2aa11b3c404968490db80951c21b8561b35482cb
Daisy <daisy776.tw@gmail.com>
2020-09-16 23:40:56 +0800

    更新首頁內容

--- src/views/front/Layout.vue
+++ src/views/front/Layout.vue
@@ -1,32 +1,22 @@
 <template>
-  <div>
+  <div class="front">
     <header>
       <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light px-4">
-          <router-link class="navbar-brand" to="/home">
+        <h1>
+          <router-link class="logo" to="/home">
             <img src="@/assets/img/logo.png" alt="Healthy bakery logo" />
           </router-link>
-          <button class="navbar-toggler" type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <router-link to="/about" class="nav-link">關於我們</router-link>
-              </li>
-              <li class="nav-item">
+        </h1>
+        <nav>
+          <div>
+            <ul>
+              <li>
                 <router-link to="/products" class="nav-link">產品列表</router-link>
               </li>
-              <li class="nav-item">
+              <li>
                 <router-link to="/cart" class="nav-link">購物車</router-link>
               </li>
-              <li class="nav-item">
+              <li>
                 <router-link to="/login" class="nav-link">後台</router-link>
               </li>
             </ul>
